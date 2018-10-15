1701afcc993ebec2134c13bec5fd5ab8d7c6ad4e
Gilberto Rojas-Vite <vdsorel@gmail.com>
2018-10-15 11:49:24 -0400

    main
    main script

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 27 15:14:25 2018
+
+@author: grvite
+"""
+import pandas as pd
+
+#Read widths of the tuning curves
+tun = pd.read_csv('/home/grvite/navigation_system/motorhead/training/data/df_tuning_widths.csv')
+tun.drop('Unnamed: 0', axis = 1, inplace = True) #check this
+
+#Read widths of the autocorrelograms
+aut = pd.read_csv('/home/grvite/navigation_system/motorhead/training/data/df_autocorrelation_widths.csv', index_col = 0)
+
+
+#Check units!
+
+#Create dataframe to store the ratio data
+result = pd.DataFrame(index = aut.index, columns = aut.columns)
+eplist = ['wake', 'sws', 'rem']
+
+for i in eplist: 
+    result.loc[i] = tun.values/aut.loc[i].values
+
+#Plot results
+    result.loc['wake'] = tun.values/aut.loc['wake'].values
